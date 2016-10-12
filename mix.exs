8d9521e0be79e42c41d9c131c88d98f260c08d44
Jae Bach Hardie <jhardie@findmypast.com>
2016-10-12 16:21:36 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GelfLogger.Mixfile do
 
   def project do
     [app: :gelf_logger,
-     version: "0.2.2",
+     version: "0.2.3",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
